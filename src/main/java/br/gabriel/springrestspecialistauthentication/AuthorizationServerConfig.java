9ed37cf10a42392fccf815a5ac5d19029818d61e
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-15 21:54:49 -0300

    22.9. Configuring the authorization server flow with password credentials and opaque tokens

---
+++ src/main/java/br/gabriel/springrestspecialistauthentication/AuthorizationServerConfig.java
@@ -0,0 +1,38 @@
+package br.gabriel.springrestspecialistauthentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.
+                inMemory()
+                    .withClient("web-client")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("password")
+                    .accessTokenValiditySeconds(60 * 60)
+                    .scopes("WRITE", "READ");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager);
+    }
+}
---
+++ src/main/java/br/gabriel/springrestspecialistauthentication/SpringRestSpecialistAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package br.gabriel.springrestspecialistauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSpecialistAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSpecialistAuthenticationApplication.class, args);
+	}
+
+}
