7a94814cb14a3e0f71390a87ed9a13573515ac32
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-16 20:52:27 -0300

    22.13. Configuring refresh token grant type on the authorization server

--- src/main/java/br/gabriel/springrestspecialistauthentication/AuthorizationServerConfig.java
+++ src/main/java/br/gabriel/springrestspecialistauthentication/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.gabriel.springrestspecialistauthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,13 +21,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
                     .withClient("web-client")
                         .secret(passwordEncoder.encode("123"))
-                        .authorizedGrantTypes("password")
+                        .authorizedGrantTypes("password", "refresh_token")
                         .accessTokenValiditySeconds(60 * 60)
                         .scopes("WRITE", "READ").and()
                     .withClient("introspect")
@@ -43,6 +47,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
-                .authenticationManager(authenticationManager);
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 }
