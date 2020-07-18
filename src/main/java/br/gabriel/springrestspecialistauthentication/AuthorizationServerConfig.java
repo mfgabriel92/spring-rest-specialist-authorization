f05d5c917b498944623badad631f5e656e1eed37
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-17 21:43:02 -0300

    22.23. Implementing PKCE support to the authorization code flow

--- src/main/java/br/gabriel/springrestspecialistauthentication/AuthorizationServerConfig.java
+++ src/main/java/br/gabriel/springrestspecialistauthentication/AuthorizationServerConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -31,49 +36,62 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
-                .inMemory()
-                    .withClient("web-client")
-                        .secret(passwordEncoder.encode("123"))
-                        .authorizedGrantTypes("password", "refresh_token")
-                        .accessTokenValiditySeconds(TOKEN_VALIDITY)
-                        .refreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY)
-                        .scopes("WRITE", "READ", "DELETE").and()
+            .inMemory()
+                .withClient("web-client")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("password", "refresh_token")
+                    .accessTokenValiditySeconds(TOKEN_VALIDITY)
+                    .refreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY)
+                    .scopes("WRITE", "READ", "DELETE").and()
 
-                    .withClient("background-app")
-                        .secret(passwordEncoder.encode("123"))
-                        .authorizedGrantTypes("client_credentials")
-                        .accessTokenValiditySeconds(TOKEN_VALIDITY)
-                        .scopes("WRITE", "READ", "DELETE").and()
+                .withClient("background-app")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("client_credentials")
+                    .accessTokenValiditySeconds(TOKEN_VALIDITY)
+                    .scopes("WRITE", "READ", "DELETE").and()
 
-                    .withClient("analytics-app")
-                        .secret(passwordEncoder.encode("123"))
-                        .authorizedGrantTypes("authorization_code")
-                        .accessTokenValiditySeconds(TOKEN_VALIDITY)
-                        .redirectUris("http://another-uri.com")
-                        .scopes("WRITE", "READ", "DELETE").and()
+                .withClient("analytics-app")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("authorization_code")
+                    .accessTokenValiditySeconds(TOKEN_VALIDITY)
+                    .redirectUris("http://another-uri.com")
+                    .scopes("WRITE", "READ", "DELETE").and()
 
-                    .withClient("webadmin")
-                        .authorizedGrantTypes("implicit")
-                        .accessTokenValiditySeconds(TOKEN_VALIDITY)
-                        .redirectUris("http://another-uri.com")
-                        .scopes("WRITE", "READ", "DELETE").and()
+                .withClient("webadmin")
+                    .authorizedGrantTypes("implicit")
+                    .accessTokenValiditySeconds(TOKEN_VALIDITY)
+                    .redirectUris("http://another-uri.com")
+                    .scopes("WRITE", "READ", "DELETE").and()
 
-                    .withClient("introspect")
-                        .secret(passwordEncoder.encode("123"))
-                        .authorizedGrantTypes("password");
+                .withClient("introspect")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("password");
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security
-                .checkTokenAccess("permitAll()");
+            .checkTokenAccess("permitAll()");
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService)
-                .reuseRefreshTokens(false);
+            .authenticationManager(authenticationManager)
+            .userDetailsService(userDetailsService)
+            .reuseRefreshTokens(false)
+            .tokenGranter(tokenGranter(endpoints));
+    }
+
+    private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+        PkceAuthorizationCodeTokenGranter pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(
+            endpoints.getTokenServices(),
+            endpoints.getAuthorizationCodeServices(),
+            endpoints.getClientDetailsService(),
+            endpoints.getOAuth2RequestFactory()
+        );
+        List<TokenGranter> granters = Arrays.asList(pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
+        return new CompositeTokenGranter(granters);
     }
 }
