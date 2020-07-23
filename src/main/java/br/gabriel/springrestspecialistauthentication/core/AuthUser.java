492d82bcb25027f68830b29a2afaffd60bf4eba4
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-23 16:17:55 -0300

    23.19. Loading permissions during authentication

--- src/main/java/br/gabriel/springrestspecialistauthentication/core/AuthUser.java
+++ src/main/java/br/gabriel/springrestspecialistauthentication/core/AuthUser.java
@@ -2,8 +2,10 @@ package br.gabriel.springrestspecialistauthentication.core;
 
 import br.gabriel.springrestspecialistauthentication.domain.TheUser;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+import java.util.Collection;
 import java.util.Collections;
 
 @Getter
@@ -12,7 +14,7 @@ public class AuthUser extends User {
 
     private final String name;
 
-    public AuthUser(TheUser theUser) {
+    public AuthUser(TheUser theUser, Collection<? extends GrantedAuthority> authorities) {
         super(theUser.getEmail(), theUser.getPassword(), Collections.emptyList());
 
         this.id = theUser.getId();
--- src/main/java/br/gabriel/springrestspecialistauthentication/core/TheUserDetails.java
+++ src/main/java/br/gabriel/springrestspecialistauthentication/core/TheUserDetails.java
@@ -3,22 +3,36 @@ package br.gabriel.springrestspecialistauthentication.core;
 import br.gabriel.springrestspecialistauthentication.domain.TheUser;
 import br.gabriel.springrestspecialistauthentication.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class TheUserDetails implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         TheUser theUser = userRepository
             .findByEmail(email)
             .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return new AuthUser(theUser);
+        return new AuthUser(theUser, getAuthorities(theUser));
+    }
+
+    private Collection<GrantedAuthority> getAuthorities(TheUser theUser) {
+        return theUser.getGroups().stream()
+            .flatMap(group -> group.getPermissions()
+                .stream().map(permission -> new SimpleGrantedAuthority(permission.getName().toUpperCase())))
+            .collect(Collectors.toSet());
     }
 }
